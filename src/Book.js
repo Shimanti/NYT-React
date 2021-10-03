8019fcba3f5e3b7d94c9d00b5bc79eb2399f709d
Shimanti <shimanti@gmail.com>
2021-10-02 22:42:54 -0700

    changed buy to image

--- src/Book.js
+++ src/Book.js
@@ -5,18 +5,17 @@ const Book = ({index, title, author, bookUrl, image, description}) => {
   return (
     <div className={style}>
       <div className="book">
-        <span className="index">{index}</span>
-        <img src={image} alt="book_image" className="image"/>
-        <section className="textual">
-          <h3 className="name">{title}</h3>
-          <h4 className="author">by {author}</h4>
-          <p className="description">{description}</p>
-        </section>
-        <a href={bookUrl} className="book-url">
-          <i className="fas fa-link"> Buy</i>
-        </a>
+          <span className="index">{index}</span>
+          <a href={bookUrl} className="book-url">
+              <img src={image} alt="book_image"/>
+          </a>
+          <section className="textual">
+            <h3 className="name">{title}</h3>
+            <h4 className="author">by {author}</h4>
+            <p className="description">{description}</p>
+          </section>
       </div>
-    </div>
+  </div>
   )
 }
 
